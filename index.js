9616c290dd27b6cfe12717b8eb91ac85214179d8
zalviandyr <zukronalviandy@gmail.com>
2021-06-20 23:58:08 +0700

    fix firebase private key

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 const firebaseConfig = {
   credential: firebase.credential.cert({
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
     projectId: process.env.FIREBASE_PROJECT_ID,
   }),
   storageBucket: process.env.FIREBASE_BUCKET,
